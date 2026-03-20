6b12cf581daec7db63ec3c1a1748a9cc9b037a89
Niels van den Dool <nielsvdd42@gmail.com>
2026-03-20 15:46:17 +0100

    initialize assignment 4

---
+++ EPA133a-G01-A4/README.md
@@ -0,0 +1,43 @@
+# README File
+
+Created by: EPA133a Group 01
+
+|        Name        | Student Number |
+|:------------------:|:---------------|
+| Niels van den Dool | 5026717        |
+|    Eli Kapteijn    | 6587380        |
+|    Nick Schreurs    | 6581951        |
+|    Lieke Wahlen    | 5179564        |
+|    Mirte Wildeboer    | 5326265        |
+
+
+## Overview
+This repository contains the model, data, and experimental setup for Assignment 3 of the EPA133a Advanced Simulation course. 
+The project focuses on building a data-driven, agent-based simulation using the Mesa framework (version 2.1.4) in Python.
+
+### Project Objective
+The primary goal of this simulation is to automatically generate a model to study the effects of bridge maintenance and unavailability on traffic throughput in Bangladesh. 
+Specifically, the model simulates goods transport (trucks) along the economically vital N1&N2 highways and all its large sideroads. 
+Because bridges play a crucial role in Bangladesh's transport network and are vulnerable to disruptions, this model analyzes how varying bridge qualities (Categories A, B, C, and D) impact overall travel delays in the road network.
+
+## How to Use
+
+**1. Prepare the Data**
+Run the `notebooks/1_data_preparation.ipynb` notebook to prepare the Bangladesh bridges dataset. Executing this notebook generates the necessary CSV input file required for the simulation.
+
+**2. Run the Simulation**
+Execute `model/model_run.py` to run the model without the visualization interface. You can configure various parameters within the script, including:
+* Specific scenarios to run
+* Number of replications (simulations)
+* Total amount of model steps
+* Starting seed
+
+**Expected Output:** The model generates a separate CSV file for each scenario and saves it to the `experiment` folder. These files log the average driving time for each replication, identified by their unique starting seeds.
+
+**3. Analyze the Results**
+Run the `notebooks/2_visualization_results.ipynb` notebook to analyze the experimental data and generate the visualizations used in our final analysis.
+
+---
+
+## Documentation
+You can find our full analysis, discussion on limitations, and write-up in the `report` folder. Happy reading!
---
+++ EPA133a-G01-A4/model/model.py
@@ -0,0 +1,251 @@
+from mesa import Model
+from mesa.time import BaseScheduler
+from mesa.space import ContinuousSpace
+from mesa.datacollection import DataCollector
+from components import Source, Sink, SourceSink, Bridge, Link, Intersection
+import pandas as pd
+from collections import defaultdict
+import networkx as nx
+
+# ---------------------------------------------------------------
+def set_lat_lon_bound(lat_min, lat_max, lon_min, lon_max, edge_ratio=0.02):
+    """
+    Set the HTML continuous space canvas bounding box (for visualization)
+    give the min and max latitudes and Longitudes in Decimal Degrees (DD)
+
+    Add white borders at edges (default 2%) of the bounding box
+    """
+
+    lat_edge = (lat_max - lat_min) * edge_ratio
+    lon_edge = (lon_max - lon_min) * edge_ratio
+
+    x_max = lon_max + lon_edge
+    y_max = lat_min - lat_edge
+    x_min = lon_min - lon_edge
+    y_min = lat_max + lat_edge
+    return y_min, y_max, x_min, x_max
+
+def compute_avg_driving_time(model):
+    """Calculates the average driving time of completed trips."""
+    if len(model.completed_trip_times) == 0:
+        return 0.0
+    # print(model.completed_trip_times)
+    return sum(model.completed_trip_times) / len(model.completed_trip_times)
+
+# ---------------------------------------------------------------
+class BangladeshModel(Model):
+    """
+    The main (top-level) simulation model
+
+    One tick represents one minute; this can be changed
+    but the distance calculation need to be adapted accordingly
+
+    Class Attributes:
+    -----------------
+    step_time: int
+        step_time = 1 # 1 step is 1 min
+
+    path_ids_dict: defaultdict
+        Key: (origin, destination)
+        Value: the shortest path (Infra component IDs) from an origin to a destination
+
+        Only straight paths in the Demo are added into the dict;
+        when there is a more complex network layout, the paths need to be managed differently
+
+    sources: list
+        all sources in the network
+
+    sinks: list
+        all sinks in the network
+
+    """
+
+    step_time = 1
+
+    file_name = '../data/data_intersectionsN1N2.csv'
+
+    def __init__(self, seed=None, x_max=500, y_max=500, x_min=0, y_min=0, scenario={'A': 0.00, 'B': 0.00, 'C':0.00, 'D':0.00}):
+
+        self.schedule = BaseScheduler(self)
+        self.running = True
+        self.path_ids_dict = defaultdict(lambda: pd.Series())
+        self.space = None
+        self.sources = []
+        self.sinks = []
+        self.scenario = scenario
+        self.completed_trip_times = []
+        self.G = nx.Graph()
+        self.generate_model()
+
+        self.datacollector = DataCollector(
+            model_reporters={"Average_Driving_Time": compute_avg_driving_time}
+        )
+
+    def generate_model(self):
+        """
+        generate the simulation model and structures the data for networkX modelling according to the csv file component information
+
+        Warning: the labels are the same as the csv column labels
+        """
+
+        df = pd.read_csv(self.file_name)
+
+        # a list of names of roads to be generated
+        roads = list(df.road.unique())
+
+        df_objects_all = []
+        for road in roads:
+            # Select all the objects on a particular road in the original order as in the cvs
+            df_objects_on_road = df[df['road'] == road]
+
+            if not df_objects_on_road.empty:
+                df_objects_all.append(df_objects_on_road)
+
+                """
+                Set the path 
+                1. get the serie of object IDs on a given road in the cvs in the original order
+                2. add the (straight) path to the path_ids_dict
+                3. put the path in reversed order and reindex
+                4. add the path to the path_ids_dict so that the vehicles can drive backwards too
+                """
+                path_ids = df_objects_on_road['id']
+                path_ids.reset_index(inplace=True, drop=True)
+                self.path_ids_dict[path_ids[0], path_ids.iloc[-1]] = path_ids
+                self.path_ids_dict[path_ids[0], None] = path_ids
+                path_ids = path_ids[::-1]
+                path_ids.reset_index(inplace=True, drop=True)
+                self.path_ids_dict[path_ids[0], path_ids.iloc[-1]] = path_ids
+                self.path_ids_dict[path_ids[0], None] = path_ids
+
+        # put back to df with selected roads so that min and max and be easily calculated
+        df = pd.concat(df_objects_all)
+        y_min, y_max, x_min, x_max = set_lat_lon_bound(
+            df['lat'].min(),
+            df['lat'].max(),
+            df['lon'].min(),
+            df['lon'].max(),
+            0.05
+        )
+
+        # ContinuousSpace from the Mesa package;
+        # not to be confused with the SimpleContinuousModule visualization
+        self.space = ContinuousSpace(x_max, y_max, True, x_min, y_min)
+        network_nodes = {"bridges": [], "sourcesinks": [], "intersections": [], "links": {}}
+        for df in df_objects_all:
+            for _, row in df.iterrows():  # index, row in ...
+
+                # create agents according to model_type
+                model_type = row['model_type'].strip()
+                agent = None
+
+                name = row['name']
+                if pd.isna(name):
+                    name = ""
+                else:
+                    name = name.strip()
+
+                if model_type == 'source':
+                    agent = Source(row['id'], self, row['length'], name, row['road'])
+                    self.sources.append(agent.unique_id)
+                elif model_type == 'sink':
+                    agent = Sink(row['id'], self, row['length'], name, row['road'])
+                    self.sinks.append(agent.unique_id)
+                elif model_type == 'sourcesink':
+                    agent = SourceSink(row['id'], self, row['length'], name, row['road'])
+                    self.sources.append(agent.unique_id)
+                    self.sinks.append(agent.unique_id)
+                    network_nodes["sourcesinks"].append((row['id'], row['length'], row['lon'], row['lat']))
+                elif model_type == 'bridge':
+                    agent = Bridge(row['id'], self, row['length'], name, row['road'], condition=row['condition'], broken_chance=self.scenario[row['condition']])
+                    network_nodes["bridges"].append((row['id'], row['length'],row['lon'], row['lat']))
+                elif model_type == 'link':
+                    agent = Link(row['id'], self, row['length'], name, row['road'])
+                    network_nodes["links"][row['id']] = row['length']
+                elif model_type == 'intersection':
+                    if not row['id'] in self.schedule._agents:
+                        agent = Intersection(row['id'], self, row['length'], name, row['road'])
+                        network_nodes["intersections"].append((row['id'], row['length'], row['lon'], row['lat']))
+
+                if agent:
+                    self.schedule.add(agent)
+                    y = row['lat']
+                    x = row['lon']
+                    self.space.place_agent(agent, (x, y))
+                    agent.pos = (x, y)
+        self.generate_network(network_nodes)
+        # pos = {}
+        # for node, data in self.G.nodes(data=True):
+        #     pos[node] = (data['lon'], data['lat'])
+
+    def generate_network(self, network_dict):
+        """
+        Generate a networkX model from the specified network_dict
+        """
+        #Add bridges as nodes to network
+        bridges = network_dict["bridges"]
+        bridges_with_length = [(bridge[0], {'weight': bridge[1], 'type': 'bridge', 'lon': bridge[2], 'lat': bridge[3]}) for bridge in bridges]
+        self.G.add_nodes_from(bridges_with_length)
+
+        sourcesinks = network_dict["sourcesinks"]
+        sourcesinks_nodes = [(sourcesink[0], {'weight': sourcesink[1], 'type': 'SoSi', 'lon': sourcesink[2], 'lat': sourcesink[3]}) for sourcesink in sourcesinks]
+        self.G.add_nodes_from(sourcesinks_nodes)
+
+        intersections = network_dict["intersections"]
+        intersections_nodes = [(intersection[0], {'weight': intersection[1], 'type': 'intersection', 'lon': intersection[2], 'lat': intersection[3]}) for intersection in intersections]
+        self.G.add_nodes_from(intersections_nodes)
+
+        links = network_dict["links"]
+
+        paths = self.path_ids_dict
+        edge_list = []
+        for path_key in paths:
+            path = paths[path_key]
+            for i in range(len(path) - 1):
+                if path.iloc[i] in links:
+                    edge_list.append((path.iloc[i-1], path.iloc[i+1], links[path.iloc[i]]))
+        self.G.add_weighted_edges_from(edge_list)
+
+
+    def get_random_route(self, source):
+        """
+        pick up a random route given an origin
+        """
+        while True:
+            # different source and sink
+            sink = self.random.choice(self.sinks)
+            if sink is not source:
+                break
+        return self.path_ids_dict[source, sink]
+
+    def get_route(self, source):
+        """
+        Picks a random sink and generates the shortest path from the specified source to this sink.
+        First looks up if a path has been generated before, otherwise generates new one from networkX model.
+        """
+        while True:
+            # different source and sink
+            sink = self.random.choice(self.sinks)
+            if sink is not source:
+                break
+        if (source, sink) in self.path_ids_dict:
+            return self.path_ids_dict[source, sink]
+        else:
+            shortest_path =  nx.shortest_path(self.G, source=source, target=sink, weight='weight')
+            self.path_ids_dict[source, sink] = shortest_path
+            return shortest_path
+
+    def get_straight_route(self, source):
+        """
+        pick up a straight route given an origin
+        """
+        return self.path_ids_dict[source, None]
+
+    def step(self):
+        """
+        Advance the simulation by one step.
+        """
+        self.schedule.step()
+        self.datacollector.collect(self)
+
+
+# EOF -----------------------------------------------------------
